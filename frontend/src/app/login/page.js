9067b92874827d571a0b30ddf4ae516f8d18f457
HarmandeepSaggu <harmansaggu909@gmail.com>
2025-06-18 18:57:16 +0530

    Update page.js

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -221,7 +221,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:4000/api/auth/login", {
+      const res = await fetch("https://app-backend-1naq.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
