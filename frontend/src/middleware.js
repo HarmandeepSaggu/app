f5d160852c85562c461dee702c46a361d1f5152b
HarmandeepSaggu <harmansaggu909@gmail.com>
2025-06-19 11:57:19 +0530

    Update middleware.js

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,16 +1,16 @@
-// middleware.js
-import { NextResponse } from 'next/server';
+// // middleware.js
+// import { NextResponse } from 'next/server';
 
-export function middleware(request) {
-  const token = request.cookies.get('token');
-   console.log("token", token)
-  // if (!token && request.nextUrl.pathname.startsWith('/chat')) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+// export function middleware(request) {
+//   const token = request.cookies.get('token');
+//    console.log("token", token)
+//   // if (!token && request.nextUrl.pathname.startsWith('/chat')) {
+//   //   return NextResponse.redirect(new URL('/login', request.url));
+//   // }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ['/chat/:path*'], // protect all /chat routes
-};
+// export const config = {
+//   matcher: ['/chat/:path*'], // protect all /chat routes
+// };
